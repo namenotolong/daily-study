4de2a6fb202bd9192d1d6245be4e12dcb6409205
huyong <huyong@huyongdeMacBook-Pro.local>
2021-01-09 22:05:09 +0800

    add some scala

--- src/main/scala/WorldCount.scala
+++ src/main/scala/WorldCount.scala
@@ -5,12 +5,12 @@ class WorldCount {
 }
 object WorldCount{
   def main(args: Array[String]): Unit = {
-    val fileName = "/Users/weidian/Desktop/test.txt";
+    val fileName = "/Users/huyong/Desktop/workHome/test.txt";
     val conf = new SparkConf().setMaster("local[*]").setAppName("wordCount")
     val context = new SparkContext(conf)
     val rdd = context.textFile(fileName)
-    var res = "";
-    rdd.flatMap(e => e.split(":")).map((_,1)).reduceByKey(_+_).foreach(print)
+    val value = rdd.flatMap(e => e.split(" ")).map((_, 1))
+    val tuples = value.reduceByKey(_ + _).collect()
   }
 
   private lazy val a : Int = {
