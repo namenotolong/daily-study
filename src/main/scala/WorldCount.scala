e31a292dc9519105ffab3fcba4b108847346b11d
namenotolong <525691990@qq.com>
2021-01-09 23:18:47 +0800

    fix

--- src/main/scala/WorldCount.scala
+++ src/main/scala/WorldCount.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.util.LongAccumulator
 import org.apache.spark.{SparkConf, SparkContext}
 
 class WorldCount {
@@ -6,15 +7,34 @@ class WorldCount {
 object WorldCount{
   def main(args: Array[String]): Unit = {
     val fileName = "/Users/huyong/Desktop/workHome/test.txt";
-    val conf = new SparkConf().setMaster("local[*]").setAppName("wordCount")
-    val context = new SparkContext(conf)
+    val context = getContext
     val rdd = context.textFile(fileName)
     val value = rdd.flatMap(e => e.split(" ")).map((_, 1))
     val tuples = value.reduceByKey(_ + _).collect()
+    context.broadcast()
+  }
+
+  def getContext: SparkContext = {
+    new SparkContext(new SparkConf().setMaster("local[*]").setAppName("wordCount"))
+  }
+
+  def testBroadCast(): Unit = {
+    val list : List[Int] = List(1,2,3,4)
+    val sc = getContext
+    val value = sc.broadcast(list)
+  }
+
+  def testAccumulator(): Unit = {
+    val sc = getContext
+    val accumulator = new LongAccumulator()
+    sc.register(accumulator, "accumulator")
+    accumulator.add(1)
   }
 
   private lazy val a : Int = {
     1 + 2
   }
+
+
 
 }
