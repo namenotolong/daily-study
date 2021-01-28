6153214dfdb69908fa5015acf0f015e1f51f0769
namenotolong <525691990@qq.com>
2021-01-28 16:52:23 +0800

    fix

--- src/main/java/com/huyong/study/TestShell.java
+++ src/main/java/com/huyong/study/TestShell.java
@@ -12,9 +12,9 @@ public class TestShell {
     public static void main(String[] args) throws IOException, InterruptedException {
         Process exec = Runtime.getRuntime().exec("sh test.sh");
         InputStream inputStream1 = exec.getInputStream();
-        /*new Thread(() -> {
+        new Thread(() -> {
             new BufferedReader(new InputStreamReader(inputStream1)).lines().forEach(System.out :: println);
-        }).start();*/
+        }).start();
         exec.waitFor();
     }
 }
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 echo 'start process'
-for (( i = 0; i < 1; ++i )); do
+for (( i = 0; i < 10; ++i )); do
   echo 'hello world'
 done
 echo 'end ...'
